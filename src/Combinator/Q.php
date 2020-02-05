ef919aa4956093d2e51c9fd01d05b4a0961e55ef
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-02-05 23:44:00 +0100

    Update combinator Q.

--- src/Combinator/Q.php
+++ src/Combinator/Q.php
@@ -13,20 +13,28 @@ use loophp\combinator\Combinator;
  * @psalm-template AType
  * @psalm-template BType
  * @psalm-template CType
+ *
+ * @psalm-immutable
  */
 final class Q extends Combinator
 {
     /**
+     * @psalm-var callable(AType): BType
+     *
      * @var callable
      */
     private $f;
 
     /**
+     * @psalm-var callable(BType): CType
+     *
      * @var callable
      */
     private $g;
 
     /**
+     * @psalm-var AType
+     *
      * @var mixed
      */
     private $x;
@@ -58,16 +66,28 @@ final class Q extends Combinator
     }
 
     /**
-     * @param callable $a
+     * @template NewAType
+     * @template NewBType
+     * @template NewCType
+     *
+     * @psalm-param callable(NewAType): NewBType $f
+     *
+     * @param callable $f
+     *
+     * @psalm-return Closure(callable(NewBType): NewCType): Closure(NewAType): NewCType
      *
      * @return Closure
      */
-    public static function on(callable $a): Closure
+    public static function on(callable $f): Closure
     {
-        return static function (callable $b) use ($a): Closure {
-            return static function ($c) use ($a, $b) {
-                return (new self($a, $b, $c))();
+        return
+            /** @psalm-param callable(NewBType): NewCType $g */
+            static function (callable $g) use ($f): Closure {
+                return
+                    /** @psalm-param NewAType $x */
+                    static function ($x) use ($f, $g) {
+                        return (new self($f, $g, $x))();
+                    };
             };
-        };
     }
 }
